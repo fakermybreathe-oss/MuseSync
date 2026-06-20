83b88842c7c48d9062094ee2b1f5ee885350a1f5
fakermybreathe-oss <fakermybreathe@github.com>
2026-06-21 01:17:48 +0800

    fix(client): update Cloudflare proxy to target correct backend port 8080

--- apps/client/functions/api/[[catchall]].js
+++ apps/client/functions/api/[[catchall]].js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
   const url = new URL(context.request.url);
   url.protocol = "http:";
   url.hostname = "207.57.131.146";
-  url.port = "50520";
+  url.port = "8080";
   
   const proxyRequest = new Request(url.toString(), context.request);
   proxyRequest.headers.set("X-Forwarded-Proto", "https");
