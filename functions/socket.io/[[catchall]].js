2a09b3dc9ff493ce577559e332929fe2f76d32cb
fakermybreathe-oss <fakermybreathe@github.com>
2026-06-21 01:22:29 +0800

    chore: move Cloudflare Functions to root just in case

---
+++ functions/socket.io/[[catchall]].js
@@ -0,0 +1,12 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  url.protocol = "http:";
+  url.hostname = "207.57.131.146";
+  url.port = "8080";
+  
+  const proxyRequest = new Request(url.toString(), context.request);
+  proxyRequest.headers.set("X-Forwarded-Proto", "https");
+  proxyRequest.headers.set("X-Real-IP", context.request.headers.get("CF-Connecting-IP") || "");
+  
+  return fetch(proxyRequest);
+}
